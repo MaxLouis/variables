f213ecf6bfecf6042a59cfcb5c21aeb61d52f13d
Max Louis <40130@longroad.ac.uk>
2014-09-12 15:48:01 +0100

    Garden Challenge

---
+++ Challenge garden costs.py
@@ -0,0 +1,9 @@
+length = float(input("What is the Length in metres?"))
+width = float(input("What is the Width in metres?"))
+
+length = length - 1
+width = width - 1
+area = length * width
+cost = area * 10
+
+print("It will cost £{0}.".format(cost))
