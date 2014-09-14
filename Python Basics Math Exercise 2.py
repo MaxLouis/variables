f8ae8ca6da1322e065bb8f7c21a102e0a3cb9dbd
Max Louis <40130@longroad.ac.uk>
2014-09-14 18:31:13 +0100

    Python Basics Math: Exercise 2

---
+++ Python Basics Math Exercise 2.py
@@ -0,0 +1,13 @@
+#Max Louis
+#Python Basics Math: Exercise 2
+#14/09/14
+
+int1 = int(input("What is your first integer?"))
+int2 = int(input("What is your second integer?"))
+int3 = int(input("What is your third integer?"))
+
+multiply = int1 * int2
+
+total = multiply / int3
+
+print("Your Result is: {0}".format(total))
