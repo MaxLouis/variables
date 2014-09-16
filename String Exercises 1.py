bb6111e5e20e80213bd58800f500ff0800cedb49
Max Louis <40130@longroad.ac.uk>
2014-09-16 11:12:11 +0100

    String Exercises 1

---
+++ String Exercises 1.py
@@ -0,0 +1,30 @@
+#Max Louis
+#Exercise: Python String Basic Exercises 1
+#16/09/14
+
+import time
+
+quote = input("Please enter a quote from someone famous")
+quote_upper = quote.upper()
+
+print("Upper case")
+print(quote.upper())
+
+time.sleep(1)
+print("")
+
+print("Lower case")
+print(quote.lower())
+
+time.sleep(1)
+print("")
+
+print("Capitalize case")
+print(quote.capitalize())
+
+time.sleep(1)
+print("")
+
+print("Title case")
+print(quote.title())
+
