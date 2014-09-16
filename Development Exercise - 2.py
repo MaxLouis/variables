c192b103113af4511e4135d468392825f938ff0a
Max Louis <40130@longroad.ac.uk>
2014-09-16 10:27:44 +0100

    Development Exercise: 2

---
+++ Development Exercise - 2.py
@@ -0,0 +1,10 @@
+#Max Louis
+#Developmet Exercise: 2
+#16/09/14
+
+fahrenheit = float(input("What is the fahrenheit?"))
+
+centigrade = (fahrenheit - 32) * (5/9)
+centigrade = round(centigrade,2)
+
+print("Your centigrade is: {0} C".format(centigrade))
